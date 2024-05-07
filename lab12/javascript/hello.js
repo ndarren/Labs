efb609ce478afd84473f236861badc430ae957fc
ndarren <ndarren2017@gmail.com>
2024-05-07 13:06:38 -0700

    lab12

---
+++ lab12/javascript/hello.js
@@ -0,0 +1,6 @@
+function displayLogin() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    console.log("Username: ", username);
+    console.log("Password: ", password);
+}
