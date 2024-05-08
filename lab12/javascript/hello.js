5d36872be4ee042e18008120a0874b77942bbe6a
Darren Nguyen <ndarren2017@gmail.com>
2024-05-07 17:02:43 -0700

    lab12.2

--- lab12/javascript/hello.js
+++ lab12/javascript/hello.js
@@ -1,6 +1,9 @@
 function displayLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+    document.getElementById("usernameDisplay").textContent = "Username: " + username;
+    document.getElementById("passwordDisplay").textContent = "Password: " + password;
+    document.getElementById("displayInfo").style.display = "block";
     console.log("Username: ", username);
     console.log("Password: ", password);
 }
