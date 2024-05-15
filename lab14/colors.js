5015b9b434d635cdc3236301568f7d0875e6231e
ndarren <ndarren2017@gmail.com>
2024-05-15 01:17:03 -0700

    lab13.4

--- lab14/colors.js
+++ lab14/colors.js
@@ -1,19 +1,6 @@
-/* 
-Use these pages to help you out with Lab 14:
-https://www.freecodecamp.org/news/javascript-array-of-objects-tutorial-how-to-create-update-and-loop-through-objects-using-js-array-methods/
-
-https://www.w3schools.com/jsref/met_node_appendchild.asp
-
-https://stackoverflow.com/questions/9422974/createelement-with-id 
-
-https://www.tutorialspoint.com/How-to-add-rows-to-a-table-using-JavaScript-DOM
-*/
-
 
 function TeamObject(schoolID, schoolLogoURL, schoolName, conferenceWL, overallWL, lastGame, scoreLast, opponentLast) {
-    // Complete this teamObject constructor function using the object model 
-    // you developed in step 1; be sure to replace "...parameter list..." with 
-    // your parameter list
+
     let team = {
         schoolID: schoolID,
         schoolLogoURL: schoolLogoURL,
@@ -23,7 +10,6 @@ function TeamObject(schoolID, schoolLogoURL, schoolName, conferenceWL, overallWL
         lastGame: lastGame,
         scoreLast: scoreLast,
         opponentLast: opponentLast
-        // Put your list of valueName: parameterName pairs here
     };
     return team;
 }
@@ -36,11 +22,6 @@ function fillArray() {
     pac12_data.push(TeamObject("CAL", "https://d7.pac-12.com/sites/default/files/styles/thumbnail/public/logo-california-color-2019.png", "CALIFORNIA", "4-5", "7-6", "W", "24-14", "USC"));
     pac12_data.push(TeamObject("UW", "https://d7.pac-12.com/sites/default/files/styles/thumbnail/public/logo-washington-color-2019.png", "WASHINGTON", "3-6", "4-8", "L", "13-40", "WSU"));
     pac12_data.push(TeamObject("SU", "https://d7.pac-12.com/sites/default/files/styles/thumbnail/public/logo-stanford-color-2019.png", "STANFORD", "2-7", "3-9", "L", "14-45", "ND"));
-    // Create *each* team's 
-    //object and push it into the pac12_data array
-    // (you're using the constructor function as a parameter to the array.push
-    // function); be sure to replace "...parameter list..." with the data 
-    // parameter-list order
 }
 
 
@@ -58,16 +39,16 @@ function getColorSelector(id) {
     } else if (id == "WSU") {
         return "cougs";
     }
-    // Write a getColorSelector function that takes an ID parameter and 
-    // returns the CSS selector to use for a particular school's row; 
-    // hint:  use branching to determine which selector to return    
 }
 
 
 function fillRow(team) {
+    //selecting corresponding team id
     var i = document.querySelector("tbody #" + getColorSelector(team.schoolID));
+    //img tag set up for src
     var img = document.createElement('img');
     img.src = team.schoolLogoURL;
+    //rest is just adding text
     var name = document.createElement('td');
     name.innerHTML = team.schoolName;
     var conf = document.createElement('td');
@@ -81,8 +62,6 @@ function fillRow(team) {
     i.appendChild(conf);
     i.appendChild(overall);
     i.appendChild(last);
-    // Write a fillRow function that takes a team object and the <td>s as 
-    // paraemters, then fills <td> elements for the current team
 }
 
 
@@ -90,18 +69,10 @@ function addTableRow(teamObj) {
     var table = document.getElementById("pac12-table");
     var tbody = table.querySelector("tbody");
     const row = document.createElement("tr");
+    //setting the id to right css id
     row.id = getColorSelector(teamObj.schoolID);
     tbody.appendChild(row);
     fillRow(teamObj);
-
-
-    // Write an addTableRow function that takes a team object as its parameter; 
-    // the function uses document.appendChild() to insert a new row to the 
-    // <tbody> element
-    // addTableRow should call getColorSelector to determine which ID to use to 
-    // apply the CSS color to the row.
-    // Hint:  Remember that in the object model, you select the object 
-    // you want to work with, then manipulate it
 }
 
 
